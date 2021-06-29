77a83027f36d970c1f8ef1a61cda32cbb6b5838d
ptith._.vi <prithivirajpanda13@gmail.com>
2021-06-29 23:54:55 +0530

    todo app created

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import './App.css';
+import TodoForm from "./TodoForm";
+import Todos from "./Todos";
+
+function App() {
+    const [todos, setTodos] = useState([
+        {
+            id:1,
+            text: "my first task",
+            isCompleted: false
+        },
+        
+    ]);
+
+    const addTodo = (todo) => {
+        setTodos([...todos, {text:todo}] );
+    };
+
+    const markComplete = (index) => {
+        const newTodos = [...todos];
+        newTodos[index].isCompleted = !newTodos[index].isCompleted;
+        setTodos(newTodos);
+    };
+
+    const editTitle = (index, title) => {
+        const newTodos = [...todos];
+        newTodos[index].text = title;
+        setTodos(newTodos);
+    };
+
+    return (
+      <div className="parent-container">
+          <div className="container top-container">
+              <div className="app">
+                  <div className="todoform">
+                      <TodoForm addTodo={addTodo} />
+                      <Todos todos={todos} markComplete={markComplete} editTitle={editTitle}/>
+                  </div>
+              </div>
+          </div>
+      </div>
+    );
+}
+
+export default App;
